2f832c2e8be30602e1d96f67bc1736e9e51c4725
Aiden <aidentboyce@gmail.com>
2023-12-03 13:25:02 -0500

    Day Four

---
+++ day_four.py
@@ -0,0 +1,13 @@
+def part_one():
+    print("Hello World")
+
+def read_input():
+    with open("inputs\input_d1.txt") as f:
+        return f.read().splitlines()
+
+def main():
+    lines = read_input()
+    
+    
+if __name__ == "__main__":
+    main()
