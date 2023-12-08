91940251844e958ded8b0f0fb3db2207929c2ed6
Aiden <aidentboyce@gmail.com>
2023-12-08 13:45:05 -0500

    Part One complete

--- day_eight.py
+++ day_eight.py
@@ -3,6 +3,8 @@ def read_input():
         return f.read().splitlines()
 
 
+# Choices = LRLRLR...
+# Paths Dictionary: Source -> (Left, Right)
 def get_paths(lines):
     choices = lines[0]
     lines.remove(choices)
@@ -10,15 +12,14 @@ def get_paths(lines):
     paths = {}
     for line in lines:
         source, destinations = line.split(" = ")
-        left, right = destinations.split(", ")
-        left = left[1:]
-        right = right[:-1]
+        left, right = destinations[1:-1].split(", ")
         destinations = (left, right)
         paths[source] = destinations
 
     return choices, paths
 
 
+# Follow from
 def follow_paths(source, end_goal, choices, paths, steps):
     if source == end_goal:
         return source, steps
@@ -26,25 +27,23 @@ def follow_paths(source, end_goal, choices, paths, steps):
         return source, steps
 
     choice = choices[0]
-    choices = choices[1:]
 
     destinations = paths[source]
     if choice == "L":
-        return follow_paths(destinations[0], end_goal, choices, paths, steps + 1)
+        return follow_paths(destinations[0], end_goal, choices[1:], paths, steps + 1)
     elif choice == "R":
-        return follow_paths(destinations[1], end_goal, choices, paths, steps + 1)
+        return follow_paths(destinations[1], end_goal, choices[1:], paths, steps + 1)
 
 
 def main():
     lines = read_input()
     choices, paths = get_paths(lines)
-    source = list(paths.keys())[0]
+    source = "AAA"
     end_goal = "ZZZ"
     steps = 0
     while source != end_goal:
         source, steps = follow_paths(source, end_goal, choices, paths, steps)
-        if steps > 1000:
-            print(steps)
+
     print(source, steps)
 
 
