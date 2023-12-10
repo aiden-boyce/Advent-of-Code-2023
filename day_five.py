07aebb753e65a34fd9223f42f426cb2a80ce5968
Aiden <aidentboyce@gmail.com>
2023-12-10 16:19:10 -0500

    day 5 be working on the test case but too slow for actual puzzle input lol

--- day_five.py
+++ day_five.py
@@ -3,6 +3,7 @@ def read_input():
         return f.read().splitlines()
 
 
+# Maps Dictionary
 def make_maps_dictionary(lines):
     maps = {}
     seeds = lines[0]
@@ -27,8 +28,23 @@ def make_maps_dictionary(lines):
     return maps
 
 
+# Sort the src_to_dst by seed
+def sort_src_to_dst(unsorted_src_to_dst, last_seed):
+    unsorted_src_to_dst = swap_sources(unsorted_src_to_dst, True)
+    src_to_dst = {}
+    i = 0
+    # Sort the sources
+    while i <= last_seed:
+        src_to_dst[i] = unsorted_src_to_dst[i]
+        i += 1
+    unsorted_src_to_dst.clear()
+
+    return src_to_dst
+
+
+# Make original src_to_dst with just seeds
 def make_src_to_dst(maps):
-    unsorted_src_to_dst = {}
+    src_to_dst = {}
     for map in maps:
         destination_range = map[0]
         source_range = map[1]
@@ -37,22 +53,39 @@ def make_src_to_dst(maps):
         while i < range_len:
             source = source_range + i
             destination = destination_range + i
-            unsorted_src_to_dst[source] = 0
+            # Just initializing Dictionary with Seeds
+            src_to_dst[source] = [source]
             i += 1
 
-    sources = list(unsorted_src_to_dst.keys())
+    sources = list(src_to_dst.keys())
     sources.sort()
     last_seed = sources[-1]
-
-    src_to_dst = {}
     i = 0
     while i <= last_seed:
-        src_to_dst[i] = [i]
+        # Initializing Dictionary with Seeds
+        if not i in src_to_dst:
+            src_to_dst[i] = [i]
         i += 1
 
+    return src_to_dst, last_seed
+
+
+# Swap original source with newest source
+def swap_sources(original_src_to_dst, seed_swap):
+    src_to_dst = {}
+    for destinations in original_src_to_dst.values():
+        # Make the new source the seed
+        if seed_swap:
+            new_source = destinations[0]
+        # Make the new source the last destination
+        else:
+            new_source = destinations[-1]
+        src_to_dst[new_source] = destinations
+    original_src_to_dst.clear()
     return src_to_dst
 
 
+# Map the seeds
 def map_the_seeds(maps, map_num, src_to_dst):
     for map in maps:
         destination_range = map[0]
@@ -68,39 +101,40 @@ def map_the_seeds(maps, map_num, src_to_dst):
     for source, destination in src_to_dst.items():
         # No element mapped so use previous element
         if len(destination) != map_num + 1:
-            destination.append(destination[-1])
+            destination.append(source)
+
+    return src_to_dst
+
+
+def part_one(maps):
+    # Make original src_to_dst with just seeds
+    src_to_dst, last_seed = make_src_to_dst(maps[1])
+    # Apply the maps
+    i = 1
+    while i < len(maps):
+        map_the_seeds(maps[i], i, src_to_dst)
+        src_to_dst = swap_sources(src_to_dst, False)
+        i += 1
+    # Sort the src_to_dst by seed
+    src_to_dst = swap_sources(src_to_dst, True)
+
+    # Get the lowest location number from given seeds
+    min_location = 2**63 - 1
+    for seed in maps[0]:
+        mapped_seed = src_to_dst[seed]
+        location = mapped_seed[-1]
+        min_location = min(min_location, location)
+
+    print(min_location)
+    return src_to_dst
 
 
 def main():
     lines = read_input()
+    # Make the maps
     maps = make_maps_dictionary(lines)
-    src_to_dst = make_src_to_dst(maps[1])
-    i = 1
-    # Go through each map
-    """
-    while i <= 7:
-        map_the_seeds(maps[i], i, src_to_dst)
-        i += 1
-    """
-    # Seed #: [Seed, Soil, Fertilizer, Water, Light, Temperature, Humidity, Location]
-    map_the_seeds(maps[1], 1, src_to_dst)
-    print(f"Seed 79: {src_to_dst[79]}")
-    print(f"Seed 14: {src_to_dst[14]}")
-    print(f"Seed 55: {src_to_dst[55]}")
-    print(f"Seed 13: {src_to_dst[13]}")
-    print()
-    map_the_seeds(maps[2], 2, src_to_dst)
-    print(f"Seed 79: {src_to_dst[79]}")
-    print(f"Seed 14: {src_to_dst[14]}")
-    print(f"Seed 55: {src_to_dst[55]}")
-    print(f"Seed 13: {src_to_dst[13]}")
-    print()
-    map_the_seeds(maps[3], 3, src_to_dst)
-    print(f"Seed 79: {src_to_dst[79]}")
-    print(f"Seed 14: {src_to_dst[14]}")  # SHOULD BE WATER = 49
-    print(f"Seed 55: {src_to_dst[55]}")  # SHOULD BE WATER = 53
-    print(f"Seed 13: {src_to_dst[13]}")  # SHOULD BE WATER = 41
-    print()
+    # Run Part One
+    part_one(maps)
 
 
 if __name__ == "__main__":
